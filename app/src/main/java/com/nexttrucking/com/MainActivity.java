9e9ad874c591b2566c35c506e004c7e6d1edcbf4
HectorHann <hector@nexttrucking.com>
2020-08-11 16:05:46 +0800

    init

---
+++ app/src/main/java/com/nexttrucking/com/MainActivity.java
@@ -0,0 +1,40 @@
+package com.nexttrucking.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button pullBt;
+    private Button burnDownBt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        pullBt = findViewById(R.id.pull_reminder);
+        pullBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LRActivity.class );
+                intent.putExtra("url1","https://pullreminders.com/installs/15793071/leaderboard?v=reviews&t%5B%5D=182191978&d=14d");
+                intent.putExtra("url2", "https://pullreminders.com/installs/15793071/leaderboard?s=comments");
+                startActivity(intent);
+
+            }
+        });
+        burnDownBt = findViewById(R.id.burn_down);
+        burnDownBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        pullBt.callOnClick();
+
+    }
+
+}
