b5eba619b0ec619bb53fdfdf6b7837c5f88b7c00
HectorHan <hector@nexttrucking.com>
2021-03-09 14:15:26 +0800

    add kibana

--- app/src/main/java/com/nexttrucking/com/BurndownChartActivity.java
+++ app/src/main/java/com/nexttrucking/com/BurndownChartActivity.java
@@ -1,166 +1,33 @@
 package com.nexttrucking.com;
 
-import android.annotation.SuppressLint;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.util.Log;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.TextView;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.nexttrucking.com.helpers.WebViewHelper;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
 public class BurndownChartActivity extends AppCompatActivity {
-    private WebView webView1, webView2, webView3, webView4;
-    private TextView textView1, textView2, textView3, textView4;
-    private String script;
-    private Map<String, String> dataMap = new HashMap<>();
-    private int mapIdx = 0;
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_burndown_chart);
         initViews();
-        initDataMap();
-        initScript();
-
         loadUrls();
-
-        new Thread(new BurndownChartActivity.MyThread()).start();
     }
-
-
-    private void initDataMap() {
-        dataMap.put("Hotpot Team", "https://nexttrucking.atlassian.net/secure/RapidBoard.jspa?rapidView=76&projectKey=APD&view=reporting&chart=burndownChart");
-        dataMap.put("Spaceport Team", "https://nexttrucking.atlassian.net/secure/RapidBoard.jspa?rapidView=113&projectKey=APD&view=reporting&chart=burndownChart");
-        dataMap.put("Omega Team", "https://nexttrucking.atlassian.net/secure/RapidBoard.jspa?rapidView=112&projectKey=APD&view=reporting&chart=burndownChart");
-        dataMap.put("Quality Eng Team", "https://nexttrucking.atlassian.net/secure/RapidBoard.jspa?rapidView=91&projectKey=PLAT&view=reporting&chart=burndownChart");
-        dataMap.put("Marvel Team", "https://nexttrucking.atlassian.net/secure/RapidBoard.jspa?rapidView=114&projectKey=APD&view=reporting&chart=burndownChart");
-    }
-
 
     private void loadUrls() {
-        loadTitleAndUrl(webView1, textView1);
-        loadTitleAndUrl(webView2, textView2);
-        loadTitleAndUrl(webView3, textView3);
-        loadTitleAndUrl(webView4, textView4);
+        webView.loadUrl("https://nexttrucking.atlassian.net/plugins/servlet/Wallboard/?dashboardId=10137&cyclePeriod=2000&transitionFx=scrollLeft&random=false");
     }
-
-
-    private void loadTitleAndUrl(WebView webView, TextView textView) {
-        mapIdx = mapIdx % dataMap.size();
-        String title = (String) dataMap.keySet().toArray()[mapIdx];
-        String url = dataMap.get(title);
-        mapIdx++;
-
-        textView.setText(title);
-        webView.loadUrl(url);
-        Log.d("NEXT", title + " | " + url);
-    }
-
-
-    private void initScript() {
-        script = "javascript:(function() { " +
-                "document.getElementsByClassName('ak-navigation-resize-button')[0].click(); " +
-                "document.getElementById('ghx-chart-content').scrollIntoView(); " +
-                "document.getElementById('ghx-chart-intro').remove(); " +
-                "document.getElementById('ghx-header').remove(); " +
-                "document.getElementById('navigation-app').remove(); " +
-                "document.getElementById('ghx-report').style.height='auto'; " +
-                "document.getElementsByClassName('legend')[0].remove(); " +
-                "document.getElementById('ghx-chart-header').remove(); " +
-                "})();";
-        Log.d("NEXT", script);
-    }
-
 
     private void initViews() {
-        webView1 = findViewById(R.id.web_left1);
-        webView2 = findViewById(R.id.web_right1);
-        webView3 = findViewById(R.id.web_left2);
-        webView4 = findViewById(R.id.web_right2);
-
-        textView1 = findViewById(R.id.title_left1);
-        textView2 = findViewById(R.id.title_right1);
-        textView3 = findViewById(R.id.title_left2);
-        textView4 = findViewById(R.id.title_right2);
-
-        WebViewHelper.setCustomWebView(webView1, this);
-        WebViewHelper.setCustomWebView(webView2, this);
-        WebViewHelper.setCustomWebView(webView3, this);
-        WebViewHelper.setCustomWebView(webView4, this);
-
-
-        webView1.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                if (url.contains("burndownChart")) {
-                    view.loadUrl(script);
-                }
-
-            }
-        });
-        webView2.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                if (url.contains("burndownChart")) {
-                    view.loadUrl(script);
-                }
-            }
-        });
-        webView3.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                if (url.contains("burndownChart")) {
-                    view.loadUrl(script);
-                }
-            }
-        });
-        webView4.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                if (url.contains("burndownChart")) {
-                    view.loadUrl(script);
-                }
-            }
-        });
-
+        webView = findViewById(R.id.web);
+        WebViewHelper.setCustomWebView(webView, this);
     }
 
-
-    public class MyThread implements Runnable {
-        @Override
-        public void run() {
-            // TODO Auto-generated method stub
-            while (true) {
-                try {
-                    Message message = new Message();
-                    handler.sendMessage(message);
-                    Thread.sleep(1000 * 60 * 10);
-                } catch (InterruptedException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    @SuppressLint("HandlerLeak")
-    Handler handler = new Handler() {
-        public void handleMessage(Message msg) {
-            loadUrls();
-            super.handleMessage(msg);
-        }
-
-    };
 }
---
+++ js/pull_request_template.md
@@ -0,0 +1,16 @@
+## Purpose
+Describe the problem or feature in addition to a link to the jira ticket.
+
+## Special Reviewers
+Who has to review this PR
+- [ ] Andrew
+- [ ] Hector
+- [ ] Tiny
+- [ ] Winter
+
+## Different platforms need to be checked
+- [ ] iOS
+- [ ] Android
+
+## Screenshots
+Add **UI** or **Amplitude** screenshots for this PR
